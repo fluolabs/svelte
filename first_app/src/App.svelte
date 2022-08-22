9b87a8180ae13cc9415e702a52835f8b839e7f42
Mike <fluolabs@gmail.com>
2022-08-22 14:11:03 -0500

    e. Each blocks

--- first_app/src/App.svelte
+++ first_app/src/App.svelte
@@ -1,17 +1,81 @@
-<!-- c. Spread props -->
+<!-- a. If blocks -->
+<!-- <script>
+	let user = { loggedIn: false };
 
-<script>
-	import Tutorial from '../../03_tutorial.svelte';
-import Info from './Info.svelte';
-import Nested from './Nested.svelte';
-
-	const pkg = {
-		name: 'svelte',
-		version: 3,
-		speed: 'blazing',
-		website: 'https://svelte.dev'
-	};
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
 </script>
 
-<Info name={pkg.name} version={pkg.version} speed={pkg.speed} website={pkg.website}/>
-<Info {...pkg}/>
+{#if user.loggedIn}
+<button on:click={toggle}>
+	Log out
+</button>
+{/if}
+
+
+{#if !user.loggedIn}
+<button on:click={toggle}>
+	Log in
+</button>
+{/if} -->
+
+<!-- b. Else blocks -->
+<!-- <br>
+{#if user.loggedIn}
+<button on:click={toggle}>
+	Log out
+</button>
+{:else}
+<button on:click={toggle}>
+	Log in
+</button>
+{/if} -->
+
+<!-- c. Else-if blocks -->
+<!-- <script>
+	let x = 7;
+</script>
+
+{#if x > 10}
+	<p>{x} is greater than 10</p>
+{:else if 5 > x}
+	<p>{x} is less than 5</p>
+{:else}
+	<p>{x} is between 5 and 10</p>
+{/if} -->
+
+<!-- d. Each blocks -->
+<script>
+	let cats = [
+		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+	];
+</script>
+
+<h1>The Famous Cats of YouTube</h1>
+
+<ul>
+	{#each cats as cat}
+		<li><a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+			{cat.name}
+		</a></li>
+	{/each}
+</ul>
+
+{#each cats as cat, i}
+	<li><a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+		{i + 1}: {cat.name}
+	</a></li>
+{/each}
+
+<ul>
+	{#each cats as { id, name }, i}
+		<li><a target="_blank" href="https://www.youtube.com/watch?v={id}">
+			{i + 1}: {name}
+		</a></li>
+	{/each}
+</ul>
+
+<!-- e. Keyed each blocks -->
